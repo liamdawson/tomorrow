7cbd055252ac3dbd9f8c29c07215253b63e9a3b8
Liam Dawson <liam@liamdawson.me>
2017-05-28 14:49:35 +1000

    begin testing core helpers

---
+++ Tomorrow.Core.UnitTests/QueueingConvenienceExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Tomorrow.Core.Abstractions;
+
+namespace Tomorrow.Core.UnitTests
+{
+    [TestClass]
+    public class QueueingConvenienceExtensions
+    {
+        private class TestClass
+        {
+            public void Nop()
+            {
+            }
+
+            public virtual void TakesInt(int takes)
+            {
+            }
+        }
+
+    [TestMethod]
+        [SuppressMessage("ReSharper", "ReturnValueOfPureMethodIsNotUsed")]
+        public async Task UsesDefaultQueue()
+        {
+            var mock = new Mock<IScheduler>(MockBehavior.Loose);
+            var job = new Mock<IQueuedJob>().Object;
+            var target = mock.Object;
+
+            await target.Schedule(job);
+            await target.Schedule(job, TimeSpan.FromSeconds(1));
+            await target.Schedule<string>(s => s.ToLower());
+            await target.Schedule<string>(s => s.ToLower(), TimeSpan.FromSeconds(1));
+
+            mock.Verify(
+                scheduler => scheduler.Schedule(Scheduler.DefaultQueueName,
+                    It.IsAny<IQueuedJob>(),
+                    It.IsAny<TimeSpan>()), Times.Exactly(4));
+        }
+
+        [TestMethod]
+        [SuppressMessage("ReSharper", "ReturnValueOfPureMethodIsNotUsed")]
+        public async Task UsesDefaultDelay()
+        {
+            var mock = new Mock<IScheduler>(MockBehavior.Loose);
+            var job = new Mock<IQueuedJob>().Object;
+            var target = mock.Object;
+
+            await target.Schedule(job);
+            await target.Schedule("queue", job);
+            await target.Schedule<string>(s => s.ToLower());
+            await target.Schedule<string>("queue", s => s.ToLower());
+
+            mock.Verify(
+                scheduler => scheduler.Schedule(It.IsAny<string>(),
+                    It.IsAny<IQueuedJob>(),
+                    TimeSpan.Zero), Times.Exactly(4));
+        }
+
+        [TestMethod]
+        [SuppressMessage("ReSharper", "ReturnValueOfPureMethodIsNotUsed")]
+        public async Task ExpressionFormCreatesCorrectJobWithNoParameters()
+        {
+            var mock = new Mock<IScheduler>(MockBehavior.Loose);
+            var target = mock.Object;
+            Action method = new TestClass().Nop;
+
+            await target.Schedule<TestClass>(t => t.Nop());
+            await target.Schedule<TestClass>("queue", t => t.Nop());
+
+            mock.Verify(
+                scheduler => scheduler.Schedule(It.IsAny<string>(),
+                    Match.Create<ActivatedInstanceMethodJob>(job => Equals(job.Method, method.GetMethodInfo())),
+                    It.IsAny<TimeSpan>()), Times.Exactly(2));
+        }
+    }
+}
---
+++ Tomorrow.Core/SchedulerExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Tomorrow.Core.Abstractions;
+
+namespace Tomorrow.Core
+{
+    public static class SchedulerExtensions
+    {
+        public static async Task Schedule(this IScheduler scheduler, IQueuedJob job)
+        {
+            await Schedule(scheduler, Scheduler.DefaultQueueName, job);
+        }
+
+        public static async Task Schedule(this IScheduler scheduler, IQueuedJob job, TimeSpan delayBy)
+        {
+            await scheduler.Schedule(Scheduler.DefaultQueueName, job, delayBy);
+        }
+
+        public static async Task Schedule(this IScheduler scheduler, string queueName, IQueuedJob job)
+        {
+            await scheduler.Schedule(queueName, job, TimeSpan.Zero);
+        }
+
+        public static async Task Schedule<T>(this IScheduler scheduler, string queueName,
+            Expression<Action<T>> expression, TimeSpan delayBy)
+        {
+            var body = expression.Body as MethodCallExpression;
+
+            if (body == null || body.Method.IsStatic)
+            {
+                throw new ArgumentException("Only non-static method invocation calls are supported for scheduling an expression.", nameof(expression));
+            }
+            
+            var newJob = new ActivatedInstanceMethodJob(body.Method, expression.Parameters.Select(pe => Expression.Lambda(pe).Compile().DynamicInvoke()));
+
+            await scheduler.Schedule(queueName, newJob, delayBy);
+        }
+
+        public static async Task Schedule<T>(this IScheduler scheduler, string queueName,
+            Expression<Action<T>> expression) => await Schedule(scheduler, queueName, expression, TimeSpan.Zero);
+
+        public static async Task Schedule<T>(this IScheduler scheduler,
+            Expression<Action<T>> expression, TimeSpan delayBy) => await Schedule(scheduler, Scheduler.DefaultQueueName, expression, delayBy);
+
+        public static async Task Schedule<T>(this IScheduler scheduler, 
+            Expression<Action<T>> expression) => await Schedule(scheduler, Scheduler.DefaultQueueName, expression, TimeSpan.Zero);
+    }
+}
